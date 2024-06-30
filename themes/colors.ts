0a710e9c992a8dc68f8c13c3ffa8db45edd45363
cornchiplick <ilsunkiss@gmail.com>
2024-06-30 23:03:40 +0900

    Feat: 기본 color, fontSize 결정

--- themes/colors.ts
+++ themes/colors.ts
@@ -1,5 +1,18 @@
 const colors = {
-  default: "#212121",
+  default: "#4594FD", // 파란 메인색
+  blue: "#005EDB", // 25,000 원
+  white: "#FFFFFF", // 유색배경 속 text
+  black: "#000000", // menu 이름
+  grey: "#4C4B50", // 회색 text
+  lightgrey: "#7F7F7F", // 날짜표시 2024.03.03
+  inactivegrey: "#9F9F9F", // dkssud03@naver.com
+  fail: "#FF3535", // validation check 실패
+  warning: "#FF6A6A", // 삭제하기
+  cancel: "#BDBDBD", // 취소
+  inactivetext: "#828282", // 저녁값
+  inactivetheme: "#F5F5F5", // 그 배경 textarea
+  infotext: "#2D6A98", // 전송되었습니다.
+  infotheme: "#D7EEF5", // 그 배경 div
 };
 
 export default colors;
--- themes/fontsizes.ts
+++ themes/fontsizes.ts
@@ -1,5 +1,9 @@
 const fontSizes = {
-  xl: "2.5rem",
+  xs: "14px", // 날짜, 안내
+  sx: "16px", // default
+  md: "18px", // 버튼 text
+  lg: "24px", // 로그인, 비밀번호 찾기 등
+  xl: "25px", // 이번엔 누가 낼 차례에요
 };
 
 export default fontSizes;
