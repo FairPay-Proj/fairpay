a03c56dece3002b9b0eed0bbebd216b71f3dd18e
cornchiplick <ilsunkiss@gmail.com>
2024-08-15 17:45:53 +0900

    Feat: Flex 가 적용된 공통 컴포넌트 FlexBox 제작

--- components/styled/styledComponent.tsx
+++ components/styled/styledComponent.tsx
@@ -1,4 +1,9 @@
+import {CSSProperties} from "react";
 import styled from "styled-components";
+
+interface FlexBoxProps {
+  sx?: CSSProperties;
+}
 
 export const Title = styled.div`
   font-size: inherit;
@@ -21,4 +26,16 @@ export const ImgWrap = styled.div`
 export const Img = styled.img`
   width: 100%;
   height: 100%;
+`;
+
+export const FlexBox = styled.div<FlexBoxProps>`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  ${({sx}) =>
+    sx &&
+    Object.entries(sx)
+      .map(([key, value]) => `${key}: ${value};`)
+      .join("")}
 `;
