71116fca4bc73bda570cceab2e74d0f165e54841
cornchiplick <ilsunkiss@gmail.com>
2024-08-15 17:46:32 +0900

    Feat: 비밀번호 찾기 페이지 진행중

--- app/(route)/findPassword/page.tsx
+++ app/(route)/findPassword/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import Caption from "@FairPay/components/Caption";
+import FormInput from "@FairPay/components/FormInput";
+import MainContent from "@FairPay/components/MainContent";
+import {FlexBox, Text, Title} from "@FairPay/components/styled/styledComponent";
+import {useForm} from "react-hook-form";
+
 export default function findPassword() {
-  return <p>비밀번호 찾기</p>;
+  const {control} = useForm();
+
+  return (
+    <MainContent
+      titlePosition="top"
+      title={
+        <Title>
+          <Text>비밀번호 찾기</Text>
+        </Title>
+      }
+      childrenStyle={{paddingTop: "3.958vw"}}
+      buttonText="로그인하러 가기">
+      <FlexBox
+        sx={{
+          gap: "0.625vw",
+        }}>
+        <FormInput
+          name="email"
+          control={control}
+          type="text"
+          label="이메일"
+          placeholder="fairpay@fairpay.com"
+          $variant="all"
+        />
+        <Caption>
+          해당 이메일로 새 비밀번호가 전송되었습니다. <br />
+          확인 후 다시 로그인해 주세요.
+        </Caption>
+      </FlexBox>
+    </MainContent>
+  );
 }
