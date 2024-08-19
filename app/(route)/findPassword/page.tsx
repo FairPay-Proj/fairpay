f6b7c330553869edde7cb688646771844e96d487
cornchiplick <ilsunkiss@gmail.com>
2024-08-19 20:53:29 +0900

    Feat: 로그인페이지 추가 진행중

--- app/(route)/findPassword/page.tsx
+++ app/(route)/findPassword/page.tsx
@@ -4,10 +4,12 @@ import Caption from "@FairPay/components/Caption";
 import FormInput from "@FairPay/components/FormInput";
 import MainContent from "@FairPay/components/MainContent";
 import {FlexBox, Text, Title} from "@FairPay/components/styled/styledComponent";
+import {useState} from "react";
 import {useForm} from "react-hook-form";
 
 export default function findPassword() {
   const {control, register} = useForm();
+  const [isSubmit, setIsSubmit] = useState(false);
 
   return (
     <MainContent
@@ -19,10 +21,14 @@ export default function findPassword() {
       }
       childrenStyle={{paddingTop: "3.958vw"}}
       buttonProps={{
-        text: "로그인하러 가기",
-        onClickEvent: () => {
-          console.error("개발해야함");
-        },
+        text: isSubmit ? "로그인하러 가기" : "찾기",
+        onClickEvent: isSubmit
+          ? () => {
+              console.error("개발해야함");
+            }
+          : () => {
+              setIsSubmit((prev) => !prev);
+            },
       }}>
       <FlexBox
         sx={{
@@ -37,10 +43,12 @@ export default function findPassword() {
           validation={{required: "fairpay@fairpay.com"}}
           register={register}
         />
-        <Caption>
-          해당 이메일로 새 비밀번호가 전송되었습니다. <br />
-          확인 후 다시 로그인해 주세요.
-        </Caption>
+        {isSubmit && (
+          <Caption>
+            해당 이메일로 새 비밀번호가 전송되었습니다. <br />
+            확인 후 다시 로그인해 주세요.
+          </Caption>
+        )}
       </FlexBox>
     </MainContent>
   );
