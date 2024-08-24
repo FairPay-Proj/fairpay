1ae127e117af555a05c4c4b6d4b86e7f7a178a18
yeooonn <rladuswjd0303@naver.com>
2024-08-24 16:21:32 +0900

    회원가입 페이지 모바일 버전 적용 완료

--- themes/fontsizes.ts
+++ themes/fontsizes.ts
@@ -6,6 +6,8 @@ const fontSizes = {
   xl: "1.302vw", // 25px 이번엔 누가 낼 차례에요
   m_xl: "6vw", // 모바일용 가장 큰 타이틀 
   m_lg: "4.5vw", // 모바일용 기본 사이즈
+  m_md: "4vw", // 모바일용 input title 사이즈
+  m_sx: "3.5vw", // 모바일용 placeholder 사이즈
 };
 
 export default fontSizes;
